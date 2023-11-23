bf58ad4c8d27ca1ffcc593eb8463ab83a65eb2d7
andyjmaclean <andyjmaclean@gmail.com>
2023-11-23 15:52:01 +0100

    MET-5449 UI Lib Consolidation (prettier)

--- projects/metis-ui-test-utils/src/lib/test-helpers.spec.ts
+++ projects/metis-ui-test-utils/src/lib/test-helpers.spec.ts
@@ -1,6 +1,9 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { async, TestBed } from '@angular/core/testing';
-import { of, throwError } from 'rxjs';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
+import { async, TestBed } from "@angular/core/testing";
+import { of, throwError } from "rxjs";
 import {
   gatherError,
   gatherValues,
@@ -8,21 +11,21 @@ import {
   getUnsubscribable,
   MockHttp,
   MockHttpRequest
-} from './test-utils';
+} from "./test-utils";
 
-describe('test helpers', () => {
-  it('should get the subscription', () => {
+describe("test helpers", () => {
+  it("should get the subscription", () => {
     const sub = getUnsubscribable();
     expect(sub).toBeTruthy();
     sub.unsubscribe();
   });
 
-  it('should gather the values', () => {
+  it("should gather the values", () => {
     const values = gatherValues(of(1, 2));
     expect(values).toEqual([1, 2]);
   });
 
-  it('should gather the values', () => {
+  it("should gather the values", () => {
     const values = gatherValuesAsync(of(1, 2));
     const sub = values.subscribe((x: Array<number>) => {
       expect(x).toEqual([1, 2]);
@@ -30,40 +33,40 @@ describe('test helpers', () => {
     sub.unsubscribe();
   });
 
-  it('should gather the errors', () => {
-    const myError = new Error('It went wrongly');
+  it("should gather the errors", () => {
+    const myError = new Error("It went wrongly");
     const result1 = gatherError(of(myError));
     const result2 = gatherError(throwError(myError));
     expect(result1).toBeFalsy();
     expect(result2).toEqual(myError);
   });
 
-  describe('mocks', () => {
+  describe("mocks", () => {
     beforeEach(async(() => {
       TestBed.configureTestingModule({
         imports: [HttpClientTestingModule]
       }).compileComponents();
     }));
 
-    it('should mock the http request', () => {
-      const fakeController = ({
-        expectOne: jasmine.createSpy().and.callFake(
-          (_: string, __: string): MockHttpRequest => {
-            return ({
+    it("should mock the http request", () => {
+      const fakeController = {
+        expectOne: jasmine
+          .createSpy()
+          .and.callFake((_: string, __: string): MockHttpRequest => {
+            return {
               request: {
                 body: {},
-                method: 'GET'
+                method: "GET"
               },
               flush: () => void 0
-            } as unknown) as MockHttpRequest;
-          }
-        ),
+            } as unknown as MockHttpRequest;
+          }),
         verify: jasmine.createSpy()
-      } as unknown) as HttpTestingController;
+      } as unknown as HttpTestingController;
 
       const mockHttp = new MockHttp(fakeController);
 
-      mockHttp.expect('GET', 'my/url/').send('success');
+      mockHttp.expect("GET", "my/url/").send("success");
       expect(fakeController.expectOne).toHaveBeenCalled();
 
       expect(fakeController.verify).not.toHaveBeenCalled();
