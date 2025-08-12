1be48bc545fd675057278b8129974ad995482abf
andyjmaclean <andyjmaclean@gmail.com>
2025-08-12 17:56:44 +0200

    MET-6740 Jest Migration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,9 +33,9 @@ export default [{
         sourceType: "module",
 
         parserOptions: {
-            project: "/home/andy/git/metis-ui-test-utils/tsconfig.json",
+            project: "./tsconfig.json",
             noWatch: true,
-            tsconfigRootDir: "/home/andy/git/metis-ui-test-utils",
+            tsconfigRootDir: "./",
         },
     },
 
